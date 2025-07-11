d28fcaa6bc59e7043e9c09fe1f74d16f6f6faf3e
amanbotx2-fr <amankumarofficial23062006@gmail.com>
2025-07-11 17:02:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+  // typing element
+    var typed = new Typed("#element", {
+        strings: [
+          "Web Developer",
+          "Web Designer",
+          "Video Editor",
+          "&amp; Human",
+        ],
+        typeSpeed: 70,
+        loop: true,
+      });
